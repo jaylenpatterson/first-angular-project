b8d4a42eec5219934ce32998ca4cd6aca8c6b7ed
Jaylen patterson <jaylenpatterson16@gmail.com>
2022-03-22 15:53:04 -0400

    use directives to style display details functionality

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,14 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '.app-servers',
   templateUrl: '/servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  styles: [`
+  
+  .past5 {
+    color:white;
+  }
+  `]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created'
   serverName = 'Testserver';
   serverCreated = false;
-  username = "";
+  displayDetails = false;
+  servers = ['Testserver', 'Testserver 2'];
+  log = []
 
   constructor() { 
     setTimeout(() => {
@@ -22,14 +30,22 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
-  onDeleteUsername() {
-    if (this.username !== "") {
-      this.username = "";
+  onDisplayDetails() {
+    if (this.displayDetails === false) {
+      this.log.push(new Date())
+      return this.displayDetails = true;
     }
+
+    this.log.push(new Date())
+    this.displayDetails = false;
   }
+
+
 }
